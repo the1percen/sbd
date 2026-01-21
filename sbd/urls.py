92f11794729f8b22ffef643fccfd81942576163d
ulageshwaran <ulageshofficial@gmail.com>
2026-01-21 13:48:03 +0530

    Final update

--- sbd/urls.py
+++ sbd/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('sbdadmin/', admin.site.urls),
     path('', include('sbdapp.urls')),
     path('favicon.ico', RedirectView.as_view(url='/static/img/favicon-32x32.png', permanent=True)),
 ]
